8797ec79be5b1b4f3f665f1342cc3ed81734f033
eborowi <eborowi@gmail.com>
2022-04-11 14:32:24 -0500

    updated js based ui for RPS branch

--- index.js
+++ index.js
@@ -4,42 +4,72 @@ let playerChoice;
 
 let computerSelection= function() {
     computerChoice=options[(Math.floor(Math.random()*3))];
-    console.log(computerChoice);
     return computerChoice;
 }
 
-let playerSelection= function(){
-    playerChoice=(prompt("Please choose rock paper or scissors")).toLowerCase();
-    console.log(playerChoice);
-    return playerChoice;
-}
+// let playerSelection= function(){
+    //     playerChoice=(prompt("Please choose rock paper or scissors")).toLowerCase();
+    //     console.log(playerChoice);
+    //     return playerChoice;
+    // }
+    
+    let textNode=document.createTextNode("")
+        document.body.appendChild(textNode);
+    
+    let rexultNode=document.createTextNode("");
+        document.body.appendChild(rexultNode);
+
+    
+
+
+    let playerWins=0
+    let computerWins=0
+
+    let pWinsCountNode=document.createTextNode("player win count="+playerWins)
+        document.body.appendChild(pWinsCountNode);
+
+    let cWinsCountNode=document.createTextNode("computer win count="+computerWins)
+        document.body.appendChild(cWinsCountNode);
+
+    let winRound= function(roundOutcome){
+        rexultNode.nodeValue=(roundOutcome);
+        playerWins++;
+        pWinsCountNode.nodeValue=("player win count="+playerWins);
+              
+    }
+
+    let loseRound= function(roundOutcome){
+        rexultNode.nodeValue=(roundOutcome);
+        computerWins++;
+        cWinsCountNode.nodeValue=("computer win count="+computerWins);
+    }
 
 let playRound=function(computerChoice, playerChoice){
     if (playerChoice===computerChoice){
-        alert("It's a tie! you both chose "+playerChoice+"!");
+        rexultNode.nodeValue=("It's a tie! you both chose "+playerChoice+"!");
     }
     else if (playerChoice==="rock"){
         if (computerChoice==="paper") {
-            alert("You lose! Paper Beats Rock");
+            loseRound("You lose! Paper Beats Rock");
         }
 
-        else (alert("you win, rock beats scissors"));
+        else (winRound("you win, rock beats scissors"));
     }
 
     else if (playerChoice==="paper"){
         if (computerChoice==="scissors") {
-            alert("You lose! Scissors Beats paper");
+            loseRound("You lose! Scissors Beats paper");
         }
 
-        else (alert("you win, paper beats rock"));
+        else (winRound("you win, paper beats rock"));
     }
 
     else if (playerChoice==="scissors"){
         if (computerChoice==="rock") {
-            alert("You lose! Rock Beats scissors");
+            loseRound("You lose! Rock Beats scissors");
         }
 
-        else (alert("you win, scissors beats paper"));
+        else (winRound("you win, scissors beats paper"));
 
     }
 
@@ -47,7 +77,38 @@ let playRound=function(computerChoice, playerChoice){
     
   
 }
-computerSelection();
-playerSelection();
-playRound(computerChoice,playerChoice)
 
+
+let buttonRock = document.getElementById("rock");
+buttonRock.addEventListener("click", rockFunction);
+
+function rockFunction (){
+    textNode.nodeValue="you chose rock";
+    return playerChoice="rock";
+    
+}
+
+let buttonPaper = document.getElementById("paper");
+buttonPaper.addEventListener("click", paperFunction);
+
+function paperFunction (){
+    textNode.nodeValue="you chose paper";
+    return playerChoice="paper";
+    }
+
+let buttonScissors = document.getElementById("scissors");
+buttonScissors.addEventListener("click", scissorsFunction);
+
+function scissorsFunction (){
+    textNode.nodeValue="you chose scissors";
+    return playerChoice="scissors";
+    
+}
+
+let buttonNewRound= document.getElementById("beginRound");
+buttonNewRound.addEventListener("click",newRoundFunction);
+
+function newRoundFunction(){
+    computerSelection();
+    playRound(computerChoice,playerChoice);
+}
